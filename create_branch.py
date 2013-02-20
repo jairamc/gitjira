7c76510272dba951a3eed82bcc6f74420f16fad0
Jairam Chandar <jairam.chandar@datasift.com>
2013-02-20 10:48:58 +0000

    Script to create branch based on Ticket in JIRA

---
+++ create_branch.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import urllib2, base64, config, sys, json, subprocess, os
+
+
+def usage():
+	return "Usage - \n\tcreate_branch.py <ticket>"
+
+def createBranch(branchname):
+	cmd = ['git', 'checkout', '-b', branchname]
+	subprocess.check_call(cmd)
+
+if (config.username == '' or config.password == ''):
+    print "Username or Password not provided"
+    sys.exit(1)
+
+if (len(sys.argv) < 2):
+	print usage()
+	sys.exit(1)
+
+ticket = sys.argv[1]
+url = 'https://jiradatasift.atlassian.net/rest/api/2/issue/' + ticket
+
+
+userHash = base64.b64encode((config.username+":"+config.password).encode('ascii'))
+opener = urllib2.build_opener()
+opener.addheaders = [('Authorization', 'Basic ' + userHash)]
+
+response = json.load(opener.open(url))
+
+print response['fields']['summary']
+
+key = response['key']
+createBranch(key)
