031446c2e4f3fd559603f1750805e46194b874bc
Michael Pitidis <michael.pitidis@datasift.com>
2013-02-21 15:00:37 +0000

    Add cli parser, do basic config validation

--- gitjira/__init__.py
+++ gitjira/__init__.py
@@ -4,25 +4,25 @@
 Module for creating/updating git branches based on Jira tickets
 """
 
-import urllib2, base64, sys, json, subprocess, os, re
+import urllib2, base64, sys, json, subprocess, re
 
-from os.path import expanduser
-home = expanduser("~")
 
 def createUserHash(username, password):
 	return base64.b64encode((username + ":" + password).encode('ascii'))
 
-def readConfig():
-	confFile = home + "/.config/gitjira/config"
-	
-	f = open(confFile, 'r')
-	return { line.split('=')[0]: line.split('=')[1] for line in f }
+def readConfig(filename, sep='='):
+    with open(filename, 'rb') as f:
+        for line in f:
+            yield [token.strip() for token in line.split(sep, 1)]
 
+def readValidateConfig(filename, keys=('base_url', 'userhash')):
+    data = dict(readConfig(filename))
+    missing = [k for k in keys if not k in data]
+    if missing:
+        raise ValueError("Fields [%s] missing from configuration file" % ', '.join(`k` for k in missing))
+    return data
 
-def callJira(ticket):
-
-	config = readConfig()
-
+def callJira(ticket, config):
 	url = config['base_url'] + '/rest/api/latest/issue/' + ticket
 	#userHash = base64.b64encode((config.username+":"+config.password).encode('ascii'))
 	opener = urllib2.build_opener()
@@ -41,8 +41,8 @@ def getBranch():
 		print "Unable to decipher branch name. Did you create this branch using git-jira tool?"
 		sys.exit(1)
 
-def createBranch(ticket):
-	response = callJira(ticket)
+def createBranch(ticket, config):
+	response = callJira(ticket, config)
 
 	key = response['key']
 	issueType = response['fields']['issuetype']['name']
@@ -51,15 +51,15 @@ def createBranch(ticket):
 	cmd = ['git', 'checkout', '-b', branchname]
 	subprocess.check_call(cmd)
 
-def commitBranch():
+def commitBranch(config):
 	branch = getBranch()
 	ticket = branch.split('/')[1]
 
-	response = callJira(ticket)
+	response = callJira(ticket, config)
 
-	key = response['key']
 	msg = response['fields']['summary']
 
 	msg = msg + "\n#TO ABORT THIS COMMIT, DELETE THE COMMIT MESSAGE ABOVE AND SAVE THIS FILE!"
 	cmd = ['git', 'commit', '-m', msg, '-e']
 	subprocess.call(cmd)
+
