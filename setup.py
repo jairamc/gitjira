2d83f82d9683c7d910eb93e5396cdcf5a7a593be
Jairam Chandar <jairam.chandar@datasift.com>
2013-02-21 17:56:48 +0000

    Version 0.2
    Better command line parser and better config

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup 
 
 setup(name='gitjira',
-    version='0.1',
+    version='0.2',
     author='Jairam Chandar',
     author_email='jairamc23@gmail.com',
-    license='GPLv2',
+    license='GPLv3',
     description='Tool to integrate Jira ticket information in git',
     scripts=['bin/gitjira'],
     packages=['gitjira'], 
